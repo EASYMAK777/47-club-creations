defa8bbfd4e279aa57102f8af413cd19fe3412ac
EASYMAK777 <jplocallawncare@gmail.com>
2020-11-24 18:49:26 -0500

    debug config Debug Android Hermes for development

--- App.js
+++ App.js
@@ -3,9 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("App executed")
+
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to 47 Club Creations!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +18,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+    
   },
 });
