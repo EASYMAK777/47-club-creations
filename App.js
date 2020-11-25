f5335f8f24b5dbfcfe13bbeb1319d7da7536d31f
EASYMAK777 <jplocallawncare@gmail.com>
2020-11-24 21:41:29 -0500

    added image

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
 
 
 function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Checkout our latest merchandise</Text>
+      <Image source={require('./assets/47cozy.png')} style={styles.img}/>
     </View>
   );
 };
@@ -19,7 +21,12 @@ const styles= StyleSheet.create({
   text: {
     color: "darkslateblue",
     fontSize: 30,
+  },
+  img: {
+    height:300,
+    width:300,
   }
+
 });
 
 export default App;
