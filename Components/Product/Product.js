e44ac82bf13a9ccb580df74727d117a2cf54263f
EASYMAK777 <jplocallawncare@gmail.com>
2020-11-26 06:23:16 -0500

    added 5 more products to product component

--- Components/Product/Product.js
+++ Components/Product/Product.js
@@ -35,6 +35,46 @@ function Product() {
         title="Press here to inquire about this product"
       />
 
+      {/* Item 4 */}
+      <Text style={styles.text}>47 Club Jogger</Text>
+      <Image source={require('../img/47club joggers.png')} style={styles.img2}/>
+      <Button 
+        onPress={() => Alert.alert("Button Pressed")}
+        title="Press here to inquire about this product"
+      />
+
+      {/* Item 5 */}
+      <Text style={styles.text}>47 Sex Lounge</Text>
+      <Image source={require('../img/47SexLounge.png')} style={styles.img2}/>
+      <Button 
+        onPress={() => Alert.alert("Button Pressed")}
+        title="Press here to inquire about this product"
+      />
+
+      {/* Item 6 */}
+      <Text style={styles.text}>47 Soccer Suit</Text>
+      <Image source={require('../img/47soccer suit.png')} style={styles.img2}/>
+      <Button 
+        onPress={() => Alert.alert("Button Pressed")}
+        title="Press here to inquire about this product"
+      />
+
+      {/* Item 7 */}
+      <Text style={styles.text}>47 Surf Hoodie</Text>
+      <Image source={require('../img/47surfhoodies.png')} style={styles.img2}/>
+      <Button 
+        onPress={() => Alert.alert("Button Pressed")}
+        title="Press here to inquire about this product"
+      />
+
+      {/* Item 8 */}
+      <Text style={styles.text}>47 Sweat Pants</Text>
+      <Image source={require('../img/47sweatpants.png')} style={styles.img2}/>
+      <Button 
+        onPress={() => Alert.alert("Button Pressed")}
+        title="Press here to inquire about this product"
+      />
+
     </View>
 
 
