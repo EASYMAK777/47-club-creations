befb19a9953df6d6506143794e23f6bc5eb68247
EASYMAK777 <jplocallawncare@gmail.com>
2020-11-28 17:11:52 -0500

    added view and text divs

--- Components/Form/Form.js
+++ Components/Form/Form.js
@@ -1,34 +1,13 @@
 import React from 'react'
-import { View, 
-        StyleSheet, 
-        Text,
-        TextInput,
-        ViewStyle,
-        TextStyle,
-        TextInputProps, 
-} from 'react-native'
+import { View, Text } from 'react-native'
 
 function Form() {
     return (
-        <View style = {styles.header}>
-            <Text style={styles.text}>{title}</Text>
 
+        <View style={styles.main__container}>
+            <Text style={styles.text}
+            
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    header: {
-        height:60,
-        padding:15,
-        backgroundColor:'darkslateblue',
-        borderWidth:1
-    },
-    text:{
-        color:"#fff",
-        fontSize:30,
-        textAlign:"center"
-    }
-});
-
 export default Form;
