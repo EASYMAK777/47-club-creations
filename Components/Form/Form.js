2ca44ccf7be3765119b37293b29fbf8efc18821f
EASYMAK777 <jplocallawncare@gmail.com>
2020-11-28 17:21:15 -0500

    setting inintial values for title, body , rating

--- Components/Form/Form.js
+++ Components/Form/Form.js
@@ -5,7 +5,9 @@ import { Formik } from 'formik';
 export default function Form(){
     return(
         <View style={styles.main__container}>
-            
+            <Formik
+                initialValues={{ title:'', body:'', rating:'' }}
+            ></Formik>
         </View>
     )
 }
