a12c7c9a7f1b7735175296adea846de635942593
EASYMAK777 <jplocallawncare@gmail.com>
2020-11-28 17:20:01 -0500

    importing formik

--- Components/Form/Form.js
+++ Components/Form/Form.js
@@ -2,5 +2,11 @@ import React from 'react';
 import { View, Text, StyleSheet, Button, TextInput, } from 'react-native';
 import { Formik } from 'formik';
 
-
+export default function Form(){
+    return(
+        <View style={styles.main__container}>
+            
+        </View>
+    )
+}
 
