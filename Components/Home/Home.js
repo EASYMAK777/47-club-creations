3ccde68516566c1de3217e57b567efdcafbeae97
EASYMAK777 <jplocallawncare@gmail.com>
2020-11-29 12:45:46 -0500

    router is fully functional

--- Components/Home/Home.js
+++ Components/Home/Home.js
@@ -6,7 +6,10 @@ import Header from '../Header/Header'
 function Home() {
     return (
         <View>
-            <Header/>
+            <Header
+                title="Welcome to 47 Club Creations" 
+                styles={style.home__header}
+            />
             <Text styles={style.home__text}>
                 Welcome to 47 Club Creations
             </Text>
@@ -19,6 +22,9 @@ const styles = StyleSheet.create({
     home__text:{
         flex:1
     },
+    home__header:{
+        flex:1
+    }
     
 })
 
