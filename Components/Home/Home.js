9b1b3351e42596bd083b0e9c0ec428ce7bf10ea4
EASYMAK777 <jplocallawncare@gmail.com>
2020-12-03 17:16:37 -0500

    switch from home page to product page functional

--- Components/Home/Home.js
+++ Components/Home/Home.js
@@ -1,48 +1,48 @@
-import React from 'react'
-import { View, } from 'react-native'
-import Header from '../Header/Header'
+// import React from 'react'
+// import { View, } from 'react-native'
+// import Header from '../Header/Header'
 
 
 
-const Home = ({ navigation }) => {
-    return (
-        <View style={{ flex:1, alignItems:"center", justifyContent:"center" }}>
-            <Header
-                title="Welcome to 47 Club Creations" 
-                styles={style.home__header}
-            />
+// const Home = ({ navigation }) => {
+//     return (
+//         <View style={{ flex:1, alignItems:"center", justifyContent:"center" }}>
+//             <Header
+//                 title="Welcome to 47 Club Creations" 
+//                 styles={style.home__header}
+//             />
             
             
-            <Button title="Check our latest merchandise"
-            onPress={()=> navigation.navigate("Products")}
-            />
+//             <Button title="Check our latest merchandise"
+//             onPress={()=> navigation.navigate("Products")}
+//             />
         
-        </View>
-    )
-}
+//         </View>
+//     )
+// }
 
 
-const styles = StyleSheet.create({
-    home__text:{
-        flex:1,
-        color:"red",
-        fontSize:30,
-        textAlign:"center"
-    },
-    home__header:{
-        flex:1,
-        color:"black",
-        fontSize:30,
-        textAlign:"center"
-    }
+// const styles = StyleSheet.create({
+//     home__text:{
+//         flex:1,
+//         color:"red",
+//         fontSize:30,
+//         textAlign:"center"
+//     },
+//     home__header:{
+//         flex:1,
+//         color:"black",
+//         fontSize:30,
+//         textAlign:"center"
+//     }
     
-})
+// })
 
 
 
 
 
-export default Home
+// export default Home
 
 
 
