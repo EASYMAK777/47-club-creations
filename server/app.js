cda2ea2f97dbe88474415774536ada1e841b3ebe
EASYMAK777 <jplocallawncare@gmail.com>
2020-12-08 12:32:49 -0500

    installed nodemon, using app.get and app.listen to test server

--- server/app.js
+++ server/app.js
@@ -2,3 +2,13 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
+
+app.get('/',(req,res)=>{
+    res.send("WElcome to node js")
+})
+
+app.listen(5000,()=>{
+    console.log("server is running")
+})
+
